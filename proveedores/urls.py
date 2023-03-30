7404ac79bc80029ca735305909cfc624ba48e289
Alexander Sanchez G <alexander_sg29@outlook.com>
2023-03-29 19:43:08 -0500

    backend proveedores.app lista

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
       path('lista/', views.proveedores, name='lista_proveedores'),
+      path('agregar/', views.agregar_proveedor, name='agregar_proveedor'),
+      path('eliminar/<int:proveedor_id>/', views.borrar_proveedor, name='borrar_proveedor'),
+      path('editar/<int:proveedor_id>/', views.editar_proveedor, name='editar_proveedor'),
 ]
--- proveedores/views.py
+++ proveedores/views.py
@@ -1,8 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Proveedor
+from .forms import ProveedorForm
 
+#Vista para agregar un nuevo proveedor
+def agregar_proveedor(request):
+    if request.method == 'POST':
+        form = ProveedorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_proveedores')
+    else:
+        form = ProveedorForm()
+    return render(request, 'proveedores/agregar_proveedor.html', 
+                  {'form': form})
 
+#Vista para ver todos los proveedores
 def proveedores(request):
     proveedores = Proveedor.objects.all()
     return render(request, 'proveedores/lista_proveedores.html', 
                   {'proveedores': proveedores})
+
+#Para poder eliminar los proveedores.
+def borrar_proveedor(request, proveedor_id):
+    proveedor = get_object_or_404(Proveedor, pk=proveedor_id)
+    proveedor.delete()
+    return redirect('lista_proveedores')
+
+# Vista para editar un proveedor existente
+def editar_proveedor(request, proveedor_id):
+    proveedor = get_object_or_404(Proveedor, pk=proveedor_id)
+
+    if request.method == 'POST':
+        form = ProveedorForm(request.POST, instance=proveedor)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_proveedores')
+    else:
+        form = ProveedorForm(instance=proveedor)
+
+    return render(request, 'proveedores/editar_proveedor.html', 
+                  {'form': form, 'proveedor': proveedor})
