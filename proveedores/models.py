b69967ff5e531fcf208008ed332e9cef41ac6be7
Alexander Sanchez G <alexander_sg29@outlook.com>
2023-03-29 19:04:00 -0500

    modelo proveedor creado y registrado

--- proveedores/models.py
+++ proveedores/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Proveedor(models.Model):
+    TIPO_OPCIONES = [
+        ('M', 'Mecánico'),
+        ('E', 'Electricista'),
+        ('S', 'Proveedor de servicios'),
+    ]
+
+    nombre = models.CharField(max_length=100)
+    celular = models.CharField(max_length=20, blank=True)
+    correo = models.EmailField(blank=True)
+    tipo = models.CharField(max_length=1, choices=TIPO_OPCIONES, blank=True)
+    descripcion = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.nombre
