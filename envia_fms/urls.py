bbe0518a866207dd166c9060ab41a46e15d1198a
Alexander Sanchez G <alexander_sg29@outlook.com>
2023-04-05 06:21:51 -0500

    Empezar de nuevo backend mtto

--- envia_fms/urls.py
+++ envia_fms/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')), #Para el dashboard
+    path('mantenimientos/', include('mantenimientos.urls')), #Para los mantenimientos
     path('proveedores/', include('proveedores.urls')), #Para los proveedores
     path('repuestos/', include('repuestos.urls')), #Para los repuestos.
     path('vehiculos/', include('vehiculos.urls')), #Para las placas
---
+++ mantenimientos/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    """ path('lista_mantenimientos/', views.lista_mantenimientos, name='lista_mantenimientos'), #ver el total de los mantenimientos
+    path('agregar_mantenimiento/', views.agregar_mantenimiento, name='agregar_mantenimiento'), #agregar nuevo mantenimiento
+    path('editar_mantenimiento/<int:mantenimiento_id>/', views.editar_mantenimiento, name='editar_mantenimiento'), #editar mantenimiento
+    path('eliminar_mantenimiento/<int:mantenimiento_id>/', views.eliminar_mantenimiento, name='eliminar_mantenimiento'), #eliminar mantenimientos """
+]
