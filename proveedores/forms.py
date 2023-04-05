deae1248c410e2efdce26c71c4ed0da266b16da0
Alexander Sanchez G <alexander_sg29@outlook.com>
2023-04-05 18:09:16 -0500

    agregar proveedor html style bs5

--- proveedores/forms.py
+++ proveedores/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from .models import Proveedor
 
+
 class ProveedorForm(forms.ModelForm):
     class Meta:
         model = Proveedor
-        fields = '__all__'
+        fields = ['nombre', 'celular', 'correo', 'tipo', 'descripcion']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'celular': forms.TextInput(attrs={'class': 'form-control'}),
+            'correo': forms.EmailInput(attrs={'class': 'form-control'}),
+            'tipo': forms.Select(attrs={'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+""" class ProveedorForm(forms.ModelForm):
+    class Meta:
+        model = Proveedor
+        fields = '__all__' """
