815bac1f84f31d31723952c2f3e6c6a648898003
Alexander Sanchez G <alexander_sg29@outlook.com>
2023-04-21 17:27:46 -0500

    calendario first impresions

--- cronograma/views.py
+++ cronograma/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def cronograma_vista(request):
-    return render(request, 'cronograma/calendar.html')
+    return render(request, 'cronograma/calendario.html')
