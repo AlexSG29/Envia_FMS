930bc87dfdf79f2ce9bb019872e4b71b2ade5e0b
Alexander Sanchez G <alexander_sg29@outlook.com>
2023-04-27 07:16:08 -0500

    littles fixes

--- proveedores/forms.py
+++ proveedores/forms.py
@@ -3,16 +3,20 @@ from .models import Proveedor
 
 
 class ProveedorForm(forms.ModelForm):
+    celular = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 
+                                                            'minlength':'10',
+                                                            'maxlength': '10'}))
+    
     class Meta:
         model = Proveedor
         fields = ['nombre', 'celular', 'correo', 'tipo', 'descripcion']
         widgets = {
             'nombre': forms.TextInput(attrs={'class': 'form-control'}),
-            'celular': forms.TextInput(attrs={'class': 'form-control'}),
             'correo': forms.EmailInput(attrs={'class': 'form-control'}),
             'tipo': forms.Select(attrs={'class': 'form-control'}),
             'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
 """ class ProveedorForm(forms.ModelForm):
     class Meta:
         model = Proveedor
