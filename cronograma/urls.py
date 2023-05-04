846ecec29ebb6e4467da5bf1d45f6b5790c010e9
Alexander Sanchez G <alexander_sg29@outlook.com>
2023-05-04 06:32:24 -0500

    calendar

--- cronograma/urls.py
+++ cronograma/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-      path('', views.cronograma_vista, name='cronograma_vista'),
-      path('calendario/', views.calendar_view, name='cronograma'),
+      path('', views.cronograma_vista, name='cronograma_vista'),#para visualizar el cronograma
 ]
--- cronograma/views.py
+++ cronograma/views.py
@@ -3,12 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def cronograma_vista(request):
     return render(request, 'cronograma/calendar.html')
-
-from vehiculos.models import Vehiculo
-
-def calendar_view(request):
-    vehiculos = Vehiculo.objects.all()
-    context = {
-        'vehiculos': vehiculos
-    }
-    return render(request, 'cronograma/calendario.html', context)
