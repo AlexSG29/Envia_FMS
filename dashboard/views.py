9515679ebeaf5f4e6c7661cf8e2dd1caabbf6ccd
Alexander Sanchez G <alexander_sg29@outlook.com>
2023-05-08 06:00:07 -0500

    deashboard y table-sm

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from mantenimientos.models import Mantenimiento
+from mantenimientos.models import Mantenimiento, RepuestoMantenimiento
+
 
 #Mostrar el dashboard.
 def dashboard(request):
     mantenimientos = Mantenimiento.objects.filter(estado=True)
-    context = {'mantenimientos': mantenimientos}
+    ultimos_repuestos = RepuestoMantenimiento.objects.filter(eliminado=False).order_by('-id')[:10]
+    context = {'mantenimientos': mantenimientos,
+               'ultimos_repuestos': ultimos_repuestos}
     return render(request, 'dashboard/dashboard.html', context)
 
