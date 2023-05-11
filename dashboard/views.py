7d1ae43ac3e98330c667dd0b0d402dbe91dcbb69
Alexander Sanchez G <alexander_sg29@outlook.com>
2023-05-11 05:19:25 -0500

    dashboard finish

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,7 +5,7 @@ from mantenimientos.models import Mantenimiento, RepuestoMantenimiento
 #Mostrar el dashboard.
 def dashboard(request):
     mantenimientos = Mantenimiento.objects.filter(estado=True)
-    ultimos_repuestos = RepuestoMantenimiento.objects.filter(eliminado=False).order_by('-id')[:10]
+    ultimos_repuestos = RepuestoMantenimiento.objects.filter(eliminado=False).order_by('-id')[:13]
     context = {'mantenimientos': mantenimientos,
                'ultimos_repuestos': ultimos_repuestos}
     return render(request, 'dashboard/dashboard.html', context)
