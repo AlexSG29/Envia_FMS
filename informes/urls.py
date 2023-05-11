08b133805bdea491e771f858163fcdc0ed0ebeda
Alexander Sanchez G <alexander_sg29@outlook.com>
2023-05-11 06:58:23 -0500

    informes layout

--- informes/urls.py
+++ informes/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.Informes, name='informes') #Vista principal para descargar informes
 ]
--- informes/views.py
+++ informes/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+#Informes dashboard
+def Informes(request):
+    return render(request, 'informes/informes.html')
