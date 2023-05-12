ab3276558e528f51ff0e7d0f681beb75844d3205
Alexander Sanchez G <alexander_sg29@outlook.com>
2023-05-12 08:05:36 -0500

    pdf fixed

--- informes/views.py
+++ informes/views.py
@@ -46,32 +46,35 @@ class InformeMantenimientosActivosView(View):
         timestamp = Paragraph(f"Generado el {fecha_actual}", styles['Timestamp'])
 
         # Creamos la tabla con los datos
-        data = [['Placa', 'Tipo', 'Fecha']]
+        data = [['Placa', 'Tipo', 'OT', 'Fecha']]
         for mantenimiento in mantenimientos:
             row = [
                 str(mantenimiento.placa),
                 mantenimiento.get_tipo_display(),
+                mantenimiento.ot,
                 mantenimiento.fecha.strftime("%d/%m/%Y"),
             ]
             data.append(row)
 
         table = Table(data)
         table.setStyle(TableStyle([
-            ('BACKGROUND', (0, 0), (-1, 0), colors.grey), # Establece el color de fondo gris para la primera fila de la tabla.
-            ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),#Esto afecta a la primera fila de la tabla.
+            ('BACKGROUND', (0, 0), (-1, 0), colors.lightblue), # Establece el color de fondo gris para la primera fila de la tabla.
+            ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),#Esto afecta a la primera fila de la tabla.
             ('ALIGN', (0, 0), (-1, -1), 'CENTER'), #Alinea todo el contenido de la tabla al centro.
             ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'), #Establece el nombre de la fuente como 'Helvetica-Bold' para la primera fila de la tabla.
             ('FONTSIZE', (0, 0), (-1, 0), 14), #Establece el tamaño de fuente para la primera fila de la tabla.
             ('BOTTOMPADDING', (0, 0), (-1, 0), 12),#Agrega un espacio de relleno en la parte inferior de la primera fila de la tabla.
             ('BACKGROUND', (0, 1), (-1, -1), colors.beige),#Establece el color de fondo beige para las filas restantes de la tabla.
             ('FONTSIZE', (0, 1), (-1, -1), 10),#Establece el tamaño de fuente para la primera el resto de la tabla.
-            ('GRID', (0, 0), (-1, -1), 1, colors.black)#Agrega una cuadrícula con un ancho de línea y un color negro a todas las celdas de la tabla.
+            ('LINEBELOW', (0, 0), (-1, 0), 1, colors.HexColor("#151414")),
+            #('GRID', (0, 0), (-1, -1), 1, colors.HexColor("#2B2B2B"))#Agrega una cuadrícula con un ancho de línea y un color negro a todas las celdas de la tabla.
         ]))
 
         # Ajustar el ancho de las columnas de la tabla
         table._argW[0] = 1.5 * inch
         table._argW[1] = 2 * inch
         table._argW[2] = 1.5 * inch
+        table._argW[3] = 1.5 * inch
 
         # Añadimos todo al PDF
         elements = [timestamp, Spacer(1, 0.25 * inch), titulo, Spacer(1, 0.25 * inch), table]
