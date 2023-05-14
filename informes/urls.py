4c923ca1eb7a06b1d0fdecf0a8eee497e3efac3a
Alexander Sanchez G <alexander_sg29@outlook.com>
2023-05-13 20:08:20 -0500

    deploy fixed

--- informes/urls.py
+++ informes/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Informes, name='informes'), #Vista principal para descargar informes
     path('informes/activos/', views.InformeMantenimientosActivosView.as_view(), name='informe_activos'),
-    path('informes/noactivos/', views.InformeMantenimientosNoActivosView.as_view(), name='informe_no_activos'),
+    path('informes/noactivos/', views.InformeMantenimientosInactivosView.as_view(), name='informe_no_activos'),
 ]
